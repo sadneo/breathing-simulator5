abf4a6c70a74004742ace2e86c491a97fd4c61f4
sadneo <haywardeel@gmail.com>
2023-01-15 23:06:18 -0500

    Changed Breathe keybind

--- src/shared/Loads/KeybindManager.lua
+++ src/shared/Loads/KeybindManager.lua
@@ -2,13 +2,14 @@ local ContextActionService = game:GetService("ContextActionService")
 
 local KeybindManager = {}
 function KeybindManager.Start()
+	print('hi')
 	ContextActionService:BindAction("Breathe", function(_actionName, inputState, _inputObject)
 		if inputState == Enum.UserInputState.Begin then
 			local ReplicatedStorage = game:GetService("ReplicatedStorage")
 			local Breathe = ReplicatedStorage.Remotes.Breathe
 			Breathe:FireServer()
 		end
-	end, false, Enum.UserInputType.MouseButton1, Enum.UserInputType.Touch)
+	end, false, Enum.KeyCode.F, Enum.UserInputType.Touch)
 end
 
 return KeybindManager
