b44badee58ccb9609d45ef30ab4159f5d57e99a2
Aidan Le <haywardeel@gmail.com>
2023-01-18 12:53:41 -0800

    Added items to ItemModule

---
+++ src/shared/Modules/ItemModule.lua
@@ -0,0 +1,18 @@
+local Items = {
+	[1] = {
+		Type = "Lung",
+		Description = "Your lungs",
+		Stats = {
+			Air = 1,
+		},
+	},
+	[1] = {
+		Type = "Lung",
+		Description = "Some better lungs",
+		Stats = {
+			Air = 3,
+		},
+	},
+}
+
+return Items
