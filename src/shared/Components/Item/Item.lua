1bfa94cbdd63911bd5b6fcd61143ac081ecd5bd4
Aidan Le <haywardeel@gmail.com>
2023-01-17 19:41:23 -0800

    Added size property to Item component

--- src/shared/Components/Item/Item.lua
+++ src/shared/Components/Item/Item.lua
@@ -3,13 +3,15 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Roact = require(ReplicatedStorage.Packages.Roact)
 
 local Item = function(props)
+	local size = props.size or UDim2.fromOffset(50, 50)
 	local text = props.text
 	local onClick = props.onClick
 	local children = props.children
 
 	return Roact.createElement("Frame", {
+		Size = size,
+
 		BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-		Size = UDim2.fromOffset(50, 50),
 	}, {
 		Roact.createFragment(children),
 		TextButton = Roact.createElement("TextButton", {
